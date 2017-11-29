d35007a96369719efd0e5baead6dc7de8bac08ca
Korune <rwxgood@126.com>
2017-11-29 23:16:19 +0800

    添加项目工程。

---
+++ KOInvoiceDisplay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KOInvoiceDisplay
+//
+//  Created by Korune on 2017/11/29.
+//  Copyright © 2017年 Korune. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KOInvoiceDisplay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KOInvoiceDisplay
+//
+//  Created by Korune on 2017/11/29.
+//  Copyright © 2017年 Korune. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
